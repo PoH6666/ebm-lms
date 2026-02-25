bf46c4ed9a8f7424b1df809a1059fd23d9e09577
PoH6666 <131944621+PoH6666@users.noreply.github.com>
2026-02-25 09:41:20 +0800

    Update database configuration for new connection

--- config/database.php
+++ config/database.php
@@ -1,11 +1,12 @@
 <?php
 ob_start();
-define('DB_HOST', 'sql303.infinityfree.com');
-define('DB_USER', 'if0_41240099');
-define('DB_PASS', 'Testsite00001');
-define('DB_NAME', 'if0_41240099_ebm_lms');
+define('DB_HOST', 'mysql.railway.internal');
+define('DB_PORT', 3306);
+define('DB_USER', 'root');
+define('DB_PASS', 'qErxeJhOQobMRAaKZBqhKGnxQIjvWzxh');
+define('DB_NAME', 'railway');
 function getConnection() {
-    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
     if ($conn->connect_error) {
         ob_end_clean();
         http_response_code(500);
